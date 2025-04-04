7a79c3445b20dcb56d07dd3ab9d279edbe876ef8
Anton Piguliak <antonpigulak@gmail.com>
2025-04-04 17:28:35 +0300

    Write main function with all calls

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from .app.io.input import *
-from .app.io.output import *
+from app.io.input import *
+from app.io.output import *
 
 def main():
-
+    user_data = input_func()
+    output_func(user_data)
+    write_to_file("data/interesting_info_to_write.txt", user_data)
+    file_data = read_with_pandas("data/interesting_info_to_read.txt")
+    output_func(file_data)
+    write_to_file("data/interesting_info_to_write_2.txt", file_data)
 
 
 if __name__ == '__main__':
